0c2c3b8406c3864461133d714110f3347150bfa5
ducnt114 <trungduc110492@gmail.com>
2023-04-26 23:43:57 +0700

    update json formater

--- src/pages/JsonFormater.tsx
+++ src/pages/JsonFormater.tsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
 const JsonFormater = () => {
+  const [formatedData, setFormatedData] = useState("");
+
+  const handleChange = (event: any) => {
+    const rawData = event.target.value;
+    if (rawData.length === 0) {
+      setFormatedData("");
+      return;
+    }
+    setFormatedData(JSON.stringify(JSON.parse(rawData), null, 4));
+  };
+
   return (
-    <div className="absolute top-20">
-      <div className="text-3xl">json formater</div>
+    <div className="w-4/5 m-auto pt-4">
+      <div className="flex flex-row space-x-4">
+        <div className="basis-1/2">
+          <textarea
+            id="json-raw"
+            cols={30}
+            rows={30}
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="paste your JSON here..."
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <div className="basis-1/2">
+          <div>
+            <textarea
+              id="json-formated"
+              cols={30}
+              rows={30}
+              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="..."
+              readOnly
+              value={formatedData}
+            ></textarea>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/JwtParser.tsx
+++ src/pages/JwtParser.tsx
@@ -23,13 +23,13 @@ const JwtParser = () => {
   };
 
   return (
-    <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <div className="columns-2">
-        <div>Encoded</div>
-        <div>Decoded</div>
+    <div className="w-4/5 m-auto pt-4">
+      <div className="flex flex-row">
+        <div className="basis-1/2 text-xl text-sky-800">Encoded </div>
+        <div className="basis-1/2 text-xl text-sky-800">Decoded</div>
       </div>
-      <div className="columns-2">
-        <div className="w-full">
+      <div className="flex flex-row space-x-4">
+        <div className="basis-1/2">
           <textarea
             id="jwt-input"
             cols={30}
@@ -39,14 +39,13 @@ const JwtParser = () => {
             onChange={handleChange}
           ></textarea>
         </div>
-
-        <div className="w-full">
+        <div className="basis-1/2">
           <div>
             <div>Header</div>
             <textarea
               id="jwt-header"
               cols={30}
-              rows={10}
+              rows={7}
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="..."
               readOnly
@@ -57,26 +56,20 @@ const JwtParser = () => {
             <textarea
               id="jwt-payload"
               cols={30}
-              rows={15}
+              rows={19}
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="..."
               readOnly
               value={jwtPayload}
             ></textarea>
-
-            {/* <div>Signature</div>
-            <textarea
-              id="jwt-input"
-              rows={4}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder={jwtSig}
-            ></textarea> */}
           </div>
         </div>
       </div>
-      {/* </div> */}
-      {/* </div> */}
-      {/* </main> */}
+      <div className="columns-2">
+        <div className="w-full"></div>
+
+        <div className="w-full"></div>
+      </div>
     </div>
   );
 };
